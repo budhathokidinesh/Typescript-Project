164690c27c1186f435cd2202933a99ccced2ed0b
Dinesh Budhathoki <physmarika@gmail.com>
2025-03-20 20:53:06 +0800

    Generic topic is finished

--- myGenerics.js
+++ myGenerics.js
@@ -1,3 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var score = [];
 var names = [];
 function indentityOne(val) {
@@ -14,3 +16,29 @@ function indentityFour(val) {
     return val;
 }
 indentityFour({ brand: "naren", type: 4 });
+function getSearchProducts(products) {
+    //do database operations
+    var myIndex = 3;
+    return products[myIndex];
+}
+var getMoreSearchedProducts = function (products) {
+    //do some dtabase operatons
+    var myIndex = 4;
+    return products[myIndex];
+};
+function anotherFunction(valOne, valTwo) {
+    return {
+        valOne: valOne,
+        valTwo: valTwo,
+    };
+}
+anotherFunction(3, { connection: "yes", username: "no", password: "jpt" });
+var Sellable = /** @class */ (function () {
+    function Sellable() {
+        this.cart = [];
+    }
+    Sellable.prototype.addToCart = function (products) {
+        this.cart.push(products);
+    };
+    return Sellable;
+}());
--- myGenerics.ts
+++ myGenerics.ts
@@ -20,3 +20,44 @@ interface Bootle {
   type: number;
 }
 indentityFour<Bootle>({ brand: "naren", type: 4 });
+
+function getSearchProducts<T>(products: T[]): T {
+  //do database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+const getMoreSearchedProducts = <T>(products: T[]): T => {
+  //do some dtabase operatons
+  const myIndex = 4;
+  return products[myIndex];
+};
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+anotherFunction(3, { connection: "yes", username: "no", password: "jpt" });
+
+interface Quize {
+  type: string;
+  name: string;
+}
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+class Sellable<T> {
+  public cart: T[] = [];
+  addToCart(products: T) {
+    this.cart.push(products);
+  }
+}
+export {};
